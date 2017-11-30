047e1aa6897e5bc5eaac89eef1fa6daffaa97645
zhanglschn <zhanglschn@163.com>
2017-11-30 11:19:20 +0800

    删除bin

---
+++ src/main/java/com/juxiz/readinglist/common/config/AmazonProperties.java
@@ -0,0 +1,10 @@
+package com.juxiz.readinglist.common.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("amazon")
+public class AmazonProperties {
+	
+}
